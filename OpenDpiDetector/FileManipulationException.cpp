336635df544d3ee9ba0b22ffc50447d591b722ee
michalbecak <michal.becak@gmail.com>
2012-05-20 02:17:10 +0300

    Update OpenDpiDetector/FileManipulationException.cpp

--- OpenDpiDetector/FileManipulationException.cpp
+++ OpenDpiDetector/FileManipulationException.cpp
@@ -1,6 +1,8 @@
 #include "FileManipulationException.h"
 
-
+///THIS FILE IS NOT NEEDED
+///EVERYTHING IS DONE IN "FileManipulationException.h"
+///THIS FILE SHOULD BE REMOVED
 FileManipulationException::FileManipulationException(void)
 {
 }
